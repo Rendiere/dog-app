925798c468d7c9915113a086d01893a46416a6fb
renierbotha <r.botha91@gmail.com>
2018-02-21 11:23:32 +0200

    Removes some large files and updates prediction api to download weights and features

--- dog_app/prediction_api.py
+++ dog_app/prediction_api.py
@@ -1,8 +1,7 @@
 import os
-import io
+import requests
 import numpy as np
 
-from PIL import Image
 from flask import Flask, render_template, request
 
 from keras.preprocessing import image
@@ -17,38 +16,69 @@ IMAGE_SIZE = (224, 224)
 
 Resnet50_model = None
 dogbreed_model = None
+dog_names = None
 features = None
 
 
-# Load the dog names
-fh = open('data/dogs_list.txt')
-dog_names = [line[4:].rstrip().replace('_', ' ') for line in fh]
-fh.close()
+def load_data():
+    def load_resnet():
+        # Load the full resnet model
+        print('Loading ResNet model...', end='')
+        global Resnet50_model
+        Resnet50_model = ResNet50(weights='imagenet', include_top=False)
+        print('done')
 
-def load_model():
-    # Load the full resnet model
-    print('Loading ResNet model...', end='')
-    global Resnet50_model
-    Resnet50_model = ResNet50(weights='data/saved_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5', include_top=False)
-    print('done')
+    def load_bottleneck():
 
-    print('Loading the bottleneck features...', end='')
-    global features
-    features = np.load('data/bottleneck_features/DogResnet50Data.npz')
-    print('done')
+        global features
 
-    # Load the model trained on dog breeds
-    # TODO: Save the complete model and not only the weights
-    # TODO: Add batch normalization
-    print('Loading the dog breeds classifier...', end='')
-    global dogbreed_model
-    dogbreed_model = Sequential()
-    dogbreed_model.add(GlobalAveragePooling2D(input_shape=features['train'].shape[1:]))
-    dogbreed_model.add(Dropout(0.2))
-    dogbreed_model.add(Dense(133, activation='softmax'))
-    dogbreed_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    dogbreed_model.load_weights('data/saved_models/weights.best.ResNet50.hdf5')
-    print('done')
+        print('Loading the bottleneck features')
+
+        # TODO: Export to config
+        bottleneck_filepath = 'data/bottleneck_features/DogResnet50Data.npz'
+        if os.path.isfile(bottleneck_filepath):
+            features = np.load('data/bottleneck_features/DogResnet50Data.npz')
+        else:
+            from tqdm import tqdm
+            print('Could not locate bottleneck features...')
+            print('Downloading bottleneck features')
+
+            # Download the file with a nice progress bar.
+            # TODO: Export to config
+            features_url = 'https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogResnet50Data.npz'
+            req = requests.get(features_url, stream=True)
+
+            file_size = int(req.headers.get('content-length', 0))
+
+            with open(bottleneck_filepath, 'wb') as f:
+                for data in tqdm(req.iter_content(32 * 1024), total=file_size, unit='kB', unit_scale=True, ncols=100):
+                    f.write(data)
+
+    def load_dogbreed():
+        # Load the model trained on dog breeds
+        # TODO: Save the complete model and not only the weights
+        # TODO: Add batch normalization
+        print('Loading the dog breeds classifier...', end='')
+        global dogbreed_model
+        dogbreed_model = Sequential()
+        dogbreed_model.add(GlobalAveragePooling2D(input_shape=features['train'].shape[1:]))
+        dogbreed_model.add(Dropout(0.2))
+        dogbreed_model.add(Dense(133, activation='softmax'))
+        dogbreed_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+        dogbreed_model.load_weights('data/saved_models/weights.best.ResNet50.hdf5')
+        print('done')
+
+    def load_dog_names():
+        global dog_names
+        # Load the dog names
+        fh = open('data/dogs_list.txt')
+        dog_names = [line[4:].rstrip().replace('_', ' ') for line in fh]
+        fh.close()
+
+    load_resnet()
+    load_bottleneck()
+    load_dogbreed()
+    load_dog_names()
 
 
 def extract_Resnet50(tensor):
@@ -68,7 +98,6 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-
     # Load the file from request
     file = request.files['image']
 
@@ -91,7 +120,7 @@ def predict():
 
 if __name__ == '__main__':
     print("Loading models, please wait while the server starts up...")
-    load_model()
+    load_data()
 
     print('\nSERVER UP AND RUNNING')
     app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=False)
